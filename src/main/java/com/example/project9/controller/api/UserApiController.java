f92c31f2f7e8cb1b8f43dbe9f875893b2a82bec1
beautiflow <1114ny@naver.com>
2023-02-22 16:30:13 +0900

    feat : 로그인 기능

--- src/main/java/com/example/project9/controller/api/UserApiController.java
+++ src/main/java/com/example/project9/controller/api/UserApiController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 @ResponseBody
 public class UserApiController {
@@ -20,6 +22,18 @@ public class UserApiController {
         System.out.println("UserApiController : save 호출됨");
         user.setRole(RoleType.USER);
         userService.join(user);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    }
+
+    @PostMapping("/Project_9/api/user/login")
+    public ResponseDto<Integer> login(@RequestBody User user, HttpSession session){
+        System.out.println("UserApiController : login 호출됨");
+        User principal = userService.login (user); // principal(접근주체)
+        
+        if(principal != null){
+            session.setAttribute("principal", principal);
+        }
+        
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
 }
--- src/main/java/com/example/project9/repository/UserRepository.java
+++ src/main/java/com/example/project9/repository/UserRepository.java
@@ -4,4 +4,8 @@ import com.example.project9.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository <User, Integer>{
+
+    // Jpa Naming 쿼리
+    // SELECT * FROM user WHERE username = ?1 AND password = ?2;
+    User findByUsernameAndPassword(String username, String password);
 }
